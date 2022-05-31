08440327bd27a99e8f8af44468f4a16427c5002b
lubo-ivanov <wulu@abv.bg>
2022-05-31 22:14:38 +0300

    fixed enums

--- healthCentre/src/main/java/projects/healthcentre/model/entity/MealProducts.java
+++ healthCentre/src/main/java/projects/healthcentre/model/entity/MealProducts.java
@@ -4,6 +4,7 @@ import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "meals_products")
 public class MealProducts extends BaseEntity {
     private Meal meal;
     private Product product;
